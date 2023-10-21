101e14076a5ae6fca3ccd18dd70761ad48a0f9c7
Matt Kirkley <matt.kirkley@gmail.com>
2023-10-21 18:52:46 -0400

    full screen map displaying

--- src/example/ExampleApp.vue
+++ src/example/ExampleApp.vue
@@ -1,12 +1,31 @@
-<script setup>
-</script>
-
 <template>
-  <div>
-    hi
+  <div class="map" ref="mapContainer">
   </div>
 </template>
 
+<script>
+  import 'leaflet';
+  import 'leaflet/dist/leaflet.css';
+
+  export default {
+    mounted() {
+      const mapEle = this.$refs.mapContainer;
+      const map = L.map(mapEle, {
+        center: [51.505, -0.09],
+        zoom: 10
+      });
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        noWrap: true,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    },
+  };
+</script>
+
 <style scoped>
+  .map {
+    height: 100vh;
+    width: 100%;
+  }
 </style>
 
