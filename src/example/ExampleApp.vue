59c787a1c989184482d59e4e08b110e47304a470
Matt Kirkley <matt.kirkley@gmail.com>
2023-10-22 15:56:12 -0400

    asfsf

--- src/example/ExampleApp.vue
+++ src/example/ExampleApp.vue
@@ -4,32 +4,42 @@
 </template>
 
 <script>
-  import 'leaflet';
-  import 'leaflet/dist/leaflet.css';
+import 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-  import '../lib/leaflet-simple-toolbar';
+import '../lib/leaflet-simple-toolbar';
 
-  export default {
-    mounted() {
-      const mapEle = this.$refs.mapContainer;
-      const map = L.map(mapEle, {
-        center: [51.505, -0.09],
-        zoom: 10
-      });
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        noWrap: true,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
+export default {
+  mounted() {
+    const mapEle = this.$refs.mapContainer;
+    const map = L.map(mapEle, {
+      center: [51.505, -0.09],
+      zoom: 10
+    });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      noWrap: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-      new L.Control.SimpleToolbar().addTo(map);
-    },
-  };
+    new L.Control.SimpleToolbar({
+      actions: [
+        {
+          iconHtml: '',
+          tooltip: 'I am a tooltip',
+          handler() {
+            alert('clicked');
+          },
+        }
+      ]
+    }).addTo(map);
+  },
+};
 </script>
 
 <style scoped>
-  .map {
-    height: 100vh;
-    width: 100%;
-  }
+.map {
+  height: 100vh;
+  width: 100%;
+}
 </style>
 
--- src/lib/leaflet-simple-toolbar.js
+++ src/lib/leaflet-simple-toolbar.js
@@ -1,15 +1,22 @@
 L.Control.SimpleToolbar = L.Control.extend({
-  options: {
-    position: 'topleft',
-  },
-
   initialize(options) {
+    if (!options.actions) {
+      throw Error('Expecting a list of actions in options');
+    }
     L.Control.prototype.initialize.call(this, options);
   },
 
   onAdd(map) {
-    const container = L.DomUtil.create('div');    
+    const container = L.DomUtil.create('div');
+    this._initializeActionListLayout(container);
     return container;
+  },
+
+  _initializeActionListLayout(container) {
+    const listEle = L.DomUtil.create('ul', null, container);
+    this.options.actions.forEach((action) => {
+      const actionLiEle = L.DomUtil.create('li', null, listEle);
+    });
   },
 });
 
