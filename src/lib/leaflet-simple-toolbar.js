ce349dc246a400843b0d7bee9c61690bb884d03f
Matthew Kirkley <matt.kirkley@gmail.com>
2023-12-06 20:21:10 +0000

    v0.0.6

--- src/lib/leaflet-simple-toolbar.js
+++ src/lib/leaflet-simple-toolbar.js
@@ -42,7 +42,9 @@ L.Control.SimpleToolbar.Action = L.Class.extend({
     L.Util.setOptions(this, options);
     this.iconHtml = this.options.iconHtml;
     this.tooltip = this.options.tooltip;
-    this.handler = this.options.handler;
+    if (this.options.handler) {
+      this.handler = this.options.handler;
+    }
   },
 
   disableControl() {
